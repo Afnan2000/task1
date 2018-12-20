0abbb5ada853dad6b4c540b9cc8a63e42dbe078f
Rana <B00123083@student.itb.ie>
2018-12-20 17:29:57 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Temp_sensor{
+
+      Temp_sensor(){
+
+      }
+
+   public:int read(){
+
+}
+};
+
+class controller{
+   Temp_sensor x;
+   controller(){
+}
+
+void run(){
+int A = x.read();
+}
+
+};
+class Humd_sensor{
+      Humd_sensor() {
+      }
+      int read(){
+
+      }
+
+     };
+
+class controller
+Humd_sensor Y;
+controller(){
+}
+
+
+void run () {
+
+int B = y.read();
+}
+
+class Data_Q;
+
+
+
+      Data_Q ()
+}
+int add (){
+
+}
+};
+
+ class controller
+ Data_Q Z;
+ controller(){
+
+ }
+ void run (){
+ int Z= add (A,B)
+
+ int main ()
+
+ }
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
